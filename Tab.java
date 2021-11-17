72b1521c8cbdc94feeaf3851fde04a673a957057
Tchuente Joel <kenmegnejoel02@gmail.com>
2021-11-17 15:04:50 +0100

    Les tableaux a une dimension en Java

---
+++ Tab.java
@@ -0,0 +1,18 @@
+
+public class Tab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Tableau à une dimension
+		int tableauEntier[] = {0,1,2,3,4,5,6,7,8,9};
+		double tableauDouble[] = {0.0,1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0};
+		char tableauCaractere[] = {'a','b','c','d','e','f','g'};
+		String tableauChaine[] = {"chaine1", "chaine2", "chaine3" ,"chaine4"};
+		// On peut déclarer des tableaux vides mais il faut definir la taille
+		int tableauEnt[] = new int[6];
+		// Ou encore
+		int[] tableauEnt2 = new int[6];
+
+	}
+
+}
