8c802643cf64c1d4a2c4d6c4df25b1cf21851d71
Tchuente Joel <kenmegnejoel02@gmail.com>
2021-11-17 15:32:58 +0100

    Utilisation et recherche dans un tableau en Java

--- Tab.java
+++ Tab.java
@@ -14,7 +14,38 @@ public class Tab {
 		int[] tableauEnt2 = new int[6];
 		//Tableau multidimensionnels
 		int nombresPremiers[][] = { {0,2,4,6,8}, {1,3,5,7,9}};
-		
+		// Utiliser et rechercher dans un tableau
+		//Un tableau débute toujours à l'indice 0!
+		//Donc si on veut appeler le premier élément d'un tableau on procède comme suit  
+		System.out.print(tableauCaractere[0]+"\n");
+		//Parcourir un tableau
+		for(int i = 0; i < tableauCaractere.length; i++)
+		{
+			System.out.print("A l'emplacement "+i+" du tableau nous avons "+tableauCaractere[i]+" \n");
+		}
+		//Parcourir un tableau multidimensionnels
+		//Avec la boucle while
+		int premiersNombres[][] = { {0,2,4,6,8},{1,3,5,7,9} }, i = 0, j = 0;
+		while (i < 2)
+		{
+		j = 0;
+		while(j < 5)
+		{
+		System.out.print(premiersNombres[i][j]);
+		j++;
+		}
+		System.out.println("");
+		i++;
+		}
+		// Avec la boucle for
+		for( i = 0; i < 2; i++)
+		{
+		for(j = 0; j < 5; j++)
+		{
+		System.out.print(premiersNombres[i][j]);
+		}
+		System.out.println("");
+		}
 
 	}
 
